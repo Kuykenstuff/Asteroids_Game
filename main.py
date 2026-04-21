472a6d64c74968ebabc804bc58b437c2e893e2d8
Kuykenstuff <Joshjbk@hotmail.com>
2026-04-21 14:45:50 -0500

    Second Commit: Completed Player variables, updated main and player files.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
@@ -9,14 +10,30 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   
+    game_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        dt = game_clock.tick(60) / 1000
         screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        
+
+
 
 
 if __name__ == "__main__":
